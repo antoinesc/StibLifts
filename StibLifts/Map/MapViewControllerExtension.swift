19302461cddd128e8b56b6b3fa662169b07581f7
antoinesc <antoinesc@gmail.com>
2017-10-31 18:55:34 +0100

    location added

---
+++ StibLifts/Map/MapViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewControllerExtension.swift
+//  StibLifts
+//
+//  Created by michael moldawski on 31/10/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import Foundation
---
+++ StibLifts/Model/Store.swift
@@ -0,0 +1,9 @@
+//
+//  Store.swift
+//  StibLifts
+//
+//  Created by michael moldawski on 31/10/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import Foundation
