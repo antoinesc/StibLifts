b346ac2e59d416f4eab710e1b323633fd5856b3b
antoinesc <antoinesc@gmail.com>
2017-11-01 19:11:43 +0100

    table view added, little bug to be fixed

---
+++ StibLifts/ListStations/ListStationsViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ListStationsViewControllerExtension.swift
+//  StibLifts
+//
+//  Created by michael moldawski on 1/11/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import Foundation
---
+++ StibLifts/ListStations/StationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StationTableViewCell.swift
+//  StibLifts
+//
+//  Created by michael moldawski on 1/11/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import UIKit
+
+class StationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
